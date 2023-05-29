9e43740b0cb298fb9d91207553039e19221b90b5
sugiartomi <sugiartomi@gmail.com>
2023-05-29 21:51:44 +0700

    }

--- payroll-ans.js
+++ payroll-ans.js
@@ -1,9 +1,9 @@
-const januaryAbsen = [
-    ["H", "H", "H", "H", "H", "H"],
-    [["A", "B"], ["L", 4], "H", ["L", 3], "H", "H"],
-    ["H", "H", "H", "T", ["A", "C"], ["A", "C"]],
-    ["H", "T", ["A", "S"], "H", "H", "H"],
-]
+let januaryAbsen = [
+	["H", "H", "H", "T", "H", "H"],
+	["H", "H", ["A", "C"], "H", "H", "H"],
+	["H", "H", "H", "H", "H", "H"],
+	["H", "H", "H", "H", ["A", "S"], "H"],
+];
 
 let hadir = 0; //16
 let sakit = 0; //1
@@ -13,31 +13,30 @@ let cuti = 0; //2
 let lembur = 0; // 7
 let gapok = 5000000;
 
-
 for (let i = 0; i < januaryAbsen.length; i++) {
-    let hasil = januaryAbsen[i];
-    for (let j = 0; j < hasil.length; j++) {
-        let hasil2 = januaryAbsen[i][j];
-        if (hasil2 == "H") {
-            hadir += 1;
-        } else if (hasil2 == "T") {
-            terlambat += 1;
-        } else if (hasil2[0] == "L") {
-            lembur += hasil2[1];
-        } else if (hasil2[0] == "A" && hasil2[1] == "B") {
-            alpa += 1;
-        } else if (hasil2[0] == "A" && hasil2[1] == "S") {
-            sakit += 1;
-        } else if (hasil2[0] == "A" && hasil2[1] == "C") {
-            cuti += 1;
-        }
-    }
+	let hasil = januaryAbsen[i];
+	for (let j = 0; j < hasil.length; j++) {
+		let hasil2 = januaryAbsen[i][j];
+		if (hasil2 == "H") {
+			hadir += 1;
+		} else if (hasil2 == "T") {
+			terlambat += 1;
+		} else if (hasil2[0] == "L") {
+			lembur += hasil2[1];
+		} else if (hasil2[0] == "A" && hasil2[1] == "B") {
+			alpa += 1;
+		} else if (hasil2[0] == "A" && hasil2[1] == "S") {
+			sakit += 1;
+		} else if (hasil2[0] == "A" && hasil2[1] == "C") {
+			cuti += 1;
+		}
+	}
 }
 
 if (hadir == 24) {
-    gapok += 500000;
+	gapok += 500000;
 } else {
-    gapok = gapok - terlambat * 50000 - alpa * 200000 + lembur * 50000;
+	gapok = gapok - terlambat * 50000 - alpa * 200000 + lembur * 50000;
 }
 
 console.log(`Absensi Bulan Ini:
@@ -47,4 +46,4 @@ Terlambat: ${terlambat}
 Alpa: ${alpa}
 Cuti: ${cuti}
 Lembur: ${lembur} jam
-Gaji yang anda terima bulan ini adalah Rp.${gapok.toLocaleString('id-ID')}`);
+Gaji yang anda terima bulan ini adalah Rp.${gapok.toLocaleString("id-ID")}`);
