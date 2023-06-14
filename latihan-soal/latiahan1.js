370756f9b7166ae07af0536adf6ae41113a2e8a3
ThessarTS <thessarts@gmail.com>
2023-06-14 21:51:06 +0700

    done

--- latihan-soal/latiahan1.js
+++ latihan-soal/latiahan1.js
@@ -15,18 +15,51 @@
 
 // output: "password low"
 
-let password = "asdV0SSD";
+let password = "";
 let angka = "0987654321";
 let huruf = "abcdefghijklmnopqrstuvwxyz";
 let special = "~!@#$%^&*";
 
-for (let i = 0; i < password.length; i++){
+password = password.toLocaleLowerCase()
+let counter = 0;
+let flag1 = false;
+let flag2 = false;
+let flag3 = false;
+
+for (let i = 0; i < password.length; i++) {
     let isiPass = password[i];
-    let flag = false;
-    for (let j of angka){
-        if (isiPass == j){
-            flag = true;
+    for (let j of angka) {
+        if (isiPass == j) {
+            flag1 = true;
+        }
+    }
+    for (let k of huruf) {
+        if (isiPass == k) {
+            flag2 = true;
+        }
+    }
+    for (let l of special) {
+        if (isiPass == l) {
+            flag3 = true;
         }
     }
 }
-console.log(flag);
+
+if (flag1 == true){
+    counter++
+}
+if (flag2 == true){
+    counter++
+}
+if (flag3 == true){
+    counter++
+}
+
+switch (counter){
+    case 3 : console.log(`password high`)
+    break;
+    case 2 : console.log(`password medium`)
+    break;
+    default : console.log(`password low`)
+}
+
